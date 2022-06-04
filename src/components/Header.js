64dbc90871c984c3cbba2e9cdf975e6462bb2857
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-04 21:28:24 +0500

    Add navlink styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,23 @@ import { FaUser } from 'react-icons/fa';
 import '../scss/Header.scss';
 
 function Header() {
+  const navLinkStyles = ({ isActive }) => ({
+    fontWeight: isActive ? 'bold' : 'normal',
+    textDecoration: isActive ? 'none' : 'underline',
+  });
   return (
     <section className="headerWrapper">
       <div className="headerContainer">
-        <span className="Bookstore-CMS">Bookstore CMS</span>
+        <span className="bookstore-CMS">Bookstore CMS</span>
         <NavLink
           to="/"
-          className={(isActive) => `nav-link${!isActive ? ' unselected' : ''}`}
+          style={navLinkStyles}
         >
           <span className="books">BOOKS</span>
         </NavLink>
         <NavLink
           to="categories"
-          className={(isActive) => `nav-link${!isActive ? ' unselected' : ''}`}
+          style={navLinkStyles}
         >
           <span className="categories">CATEGORIES</span>
         </NavLink>
