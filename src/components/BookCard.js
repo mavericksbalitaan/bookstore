e235c54d81d68156613ae2864a8c8e1c2f7a4eb8
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-05 10:11:06 +0500

    Add a functionality for Remove button click

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Progress } from 'antd';
+import { useDispatch } from 'react-redux';
+import { remBook } from '../redux/books/books';
 import '../scss/BookCard.scss';
 
 function BookCard({ book }) {
+  const dispatch = useDispatch();
   return (
     <section className="bookCardWrapper">
       <div className="bookCardContainer">
@@ -14,7 +17,7 @@ function BookCard({ book }) {
           <span className="comments">Comments</span>
           {' '}
           |
-          <span className="remove">Remove</span>
+          <button type="button" className="remove" onClick={() => dispatch(remBook(book.item_id))}>Remove</button>
           {' '}
           |
           <span className="edit">Edit</span>
@@ -63,7 +66,7 @@ function BookCard({ book }) {
 
 BookCard.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.string,
+    item_id: PropTypes.string,
     title: PropTypes.string,
     author: PropTypes.string,
     category: PropTypes.string,
